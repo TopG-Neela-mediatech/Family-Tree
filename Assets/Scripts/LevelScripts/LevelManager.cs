3581684683b78ef05f4ebb27aa1c12641364b108
Chet2k <chetanr@neelafilm.com>
2024-12-31 12:34:24 +0530

    added api scripts to levelManager

--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -7,15 +7,37 @@ namespace TMKOC.FamilyTree
         [SerializeField] private Transform treeParent;
         [SerializeField] private Transform familyMemeberParent;
         [SerializeField] private LevelSO[] levels;
+        [SerializeField] private int gameID;
+        private GameCategoryDataManager gameCategoryDataManager;
+        private UpdateCategoryApiManager updateCategoryApiManager;
+        private int currentLevelIndex;
+
 
 
         private void Awake()
         {
-            
+            #region GameID
+#if PLAYSCHOOL_MAIN
+             // assign varaible in this to get the  game ID from main app
+             gameID  = PlayerPrefs.GetInt("currentGameId");
+#endif
+            #endregion
+            gameCategoryDataManager = new GameCategoryDataManager(gameID, PlayerPrefs.GetString("currentGameName", "a"));
+            updateCategoryApiManager = new UpdateCategoryApiManager(gameID);
+            SetCurrentLevelIndex();
         }
         private void Start()
         {
-            
+
+        }
+        private void SetCurrentLevelIndex()
+        {
+            currentLevelIndex = gameCategoryDataManager.GetCompletedLevel;
+            if (currentLevelIndex > levels.Length - 1)
+            {
+                currentLevelIndex = 0;
+                gameCategoryDataManager.SaveLevel(currentLevelIndex, levels.Length);
+            }
         }
     }
 }
