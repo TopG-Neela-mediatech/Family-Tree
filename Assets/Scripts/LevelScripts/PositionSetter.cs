d4350d1de887b9f177ead055a28d6676008f216f
Chet2k <chetanr@neelafilm.com>
2025-01-06 12:19:41 +0530

    fixed aspect ratio positioning

---
+++ Assets/Scripts/LevelScripts/PositionSetter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMKOC.FamilyTree
+{
+    public class PositionSetter : MonoBehaviour
+    {
+        [SerializeField] private Transform familyMemberParentTransform;
+        [SerializeField] private Transform treeParentTransform;
+
+        private void Awake()
+        {
+            SetPosition(DetectAspectRatio());
+        }
+        private float DetectAspectRatio()
+        {
+            float screenAspect = (float)Screen.width / (float)Screen.height;
+            return screenAspect;
+        }
+        private void SetPosition(float screenAspect)
+        {
+            if (screenAspect < 1.4f)
+            {
+                familyMemberParentTransform.position = new Vector3(-3.5f, 0, 0);
+                treeParentTransform.position = new Vector3(2.6f, 0, 0);
+            }
+            else//16:9
+            {
+                familyMemberParentTransform.position = new Vector3(-7f, 0, 0);
+                treeParentTransform.position = new Vector3(1f, 0f, 0f);
+            }
+        }
+    }
+}
