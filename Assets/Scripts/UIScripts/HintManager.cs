fb7be835fa33cee9af674189f654d08083ba1bd2
Chet2k <chetanr@neelafilm.com>
2025-01-06 13:00:12 +0530

    Update HintManager.cs

--- Assets/Scripts/UIScripts/HintManager.cs
+++ Assets/Scripts/UIScripts/HintManager.cs
@@ -9,33 +9,33 @@ namespace TMKOC.FamilyTree
     public class HintManager : MonoBehaviour
     {
         [SerializeField] private Transform HintParentTransform;
-        [SerializeField] private Button HintButton;
+       // [SerializeField] private Button HintButton;
 
 
         private void Start()
         {
-            HintButton.onClick.AddListener(OnHintClick);
-            GameManager.Instance.OnLevelStart += ScaleHintParent;
-            GameManager.Instance.OnLevelWin += DeScaleHintParent;
-            GameManager.Instance.OnLevelLose += DeScaleHintParent;
+            //HintButton.onClick.AddListener(OnHintClick);
+            //GameManager.Instance.OnLevelStart += ScaleHintParent;
+            //GameManager.Instance.OnLevelWin += DeScaleHintParent;
+            //GameManager.Instance.OnLevelLose += DeScaleHintParent;
         }       
-        private void DeScaleHintParent()
+        /*private void DeScaleHintParent()
         {
             HintParentTransform.DOScale(0f, 0.5f);
         }
         private void ScaleHintParent()
         {
             HintParentTransform.DOScale(1f, 1f);
-        }
-        private void OnHintClick()
+        }*/
+        public void ActivateHint(Transform member , Transform treeSpot)
         {
             
         }      
         private void OnDestroy()
         {
-            GameManager.Instance.OnLevelStart -= ScaleHintParent;
-            GameManager.Instance.OnLevelWin -= DeScaleHintParent;
-            GameManager.Instance.OnLevelLose -= DeScaleHintParent;
+            //GameManager.Instance.OnLevelStart -= ScaleHintParent;
+            //GameManager.Instance.OnLevelWin -= DeScaleHintParent;
+            //GameManager.Instance.OnLevelLose -= DeScaleHintParent;
         }
     }
 }
