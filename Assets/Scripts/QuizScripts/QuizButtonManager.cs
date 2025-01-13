4e0bb78be8dff3d394c00ca33b49825709335c29
Chet2k <chetanr@neelafilm.com>
2025-01-13 14:09:07 +0530

    made quiz question to load next questions

--- Assets/Scripts/QuizScripts/QuizButtonManager.cs
+++ Assets/Scripts/QuizScripts/QuizButtonManager.cs
@@ -1,17 +1,33 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TMKOC.FamilyTree
 {
     public class QuizButtonManager : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI optionText;
+        [SerializeField] private Image correctImage;
+        [SerializeField] private Image incorrectImage;
+
+
         public int value { get; private set; }
+
+
+        public void EnableCorrectImage()=>correctImage.enabled = true;
+        public void EnableIncorrectImage()=>incorrectImage.enabled = true;
+
 
         public void SetData(int val, string optionText)
         {
             value = val;
             this.optionText.text = optionText;
+            DisableColorTint();
+        }
+        private void DisableColorTint()
+        {
+            correctImage.enabled = false;
+            incorrectImage.enabled = false;
         }
     }
 }
--- Assets/Scripts/QuizScripts/QuizManager.cs
+++ Assets/Scripts/QuizScripts/QuizManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,7 +16,7 @@ namespace TMKOC.FamilyTree
         private int levelIndex;
         private int questionNumber;
         private const int correctOption = 1;//correct answer always one
-        public event Action OnOptionSelected;
+        //public event Action OnOptionSelected;
 
 
         private void Start()
@@ -46,6 +47,36 @@ namespace TMKOC.FamilyTree
             }
             return null;
         }
+        private void LoadNextQuestion()
+        {
+           StartCoroutine(LoadNextQuestionAfterDelay());                
+        }
+        private IEnumerator LoadNextQuestionAfterDelay()
+        { 
+            yield return new WaitForSeconds(2.5f);
+            questionNumber++;
+            if(questionNumber > 2)
+            {
+                GameManager.Instance.InvokeLevelWin();
+                questionNumber = 0;
+                yield break;
+            }
+            StartQuiz();
+        }
+        private void DisableButtons()
+        {
+            foreach (var button in quizButtons)
+            {
+                button.QuizButton.enabled = false;
+            }
+        }
+        private void EnableButtons()
+        {
+            foreach (var button in quizButtons)
+            {
+                button.QuizButton.enabled = true;
+            }
+        }
         private void StartQuiz()
         {
             QuizSO currentQuizSO = GetQuizData();
@@ -54,24 +85,29 @@ namespace TMKOC.FamilyTree
                 QuizData currentQuizData = currentQuizSO.quizSet[questionNumber];
                 SetQuestion(currentQuizData);
                 SetOptions(currentQuizData);
+                EnableButtons();
             }
             else
             {
                 Debug.Log("Quiz not Found");
             }
         }
-        private void CheckIfCorrect(int value)
+        private void CheckIfCorrect(QuizButtonManager qbManager)
         {
-            int optionSelected = value;
+            DisableButtons();
+            int optionSelected = qbManager.value;
             if (optionSelected == correctOption)
             {
                 Debug.Log("Correct");
+                qbManager.EnableCorrectImage();
             }
             else
             {
                 Debug.Log("Incorrect");
+                qbManager.EnableIncorrectImage();
             }
-            OnOptionSelected?.Invoke();
+            LoadNextQuestion();
+            //OnOptionSelected?.Invoke();
         }
         private void SetQuestion(QuizData currentQuizdata)
         {
@@ -83,9 +119,10 @@ namespace TMKOC.FamilyTree
             {
                 int value = currentQuizdata.options[i].value;
                 string name = currentQuizdata.options[i].name;
-                quizButtons[i].buttonManager.SetData(value, name);
+                QuizButtonManager currentQBManager = quizButtons[i].buttonManager;
+                currentQBManager.SetData(value, name);
                 quizButtons[i].QuizButton.onClick.RemoveAllListeners();
-                quizButtons[i].QuizButton.onClick.AddListener(() => CheckIfCorrect(value));
+                quizButtons[i].QuizButton.onClick.AddListener(() => CheckIfCorrect(currentQBManager));
             }
         }
         private void OnDestroy()
