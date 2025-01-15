7a5d2c08053de9d7bf4af0af8999a653b2b998e4
Chet2k <chetanr@neelafilm.com>
2025-01-15 17:44:37 +0530

    added leave particle effect

--- Assets/Scripts/LevelScripts/PositionSetter.cs
+++ Assets/Scripts/LevelScripts/PositionSetter.cs
@@ -23,15 +23,15 @@ namespace TMKOC.FamilyTree
             {
                 familyMemberParentTransform.position = new Vector3(-3.5f, 0.3f, 0);
                 treeParentTransform.position = new Vector3(2.1f, -0.15f, 0);
-                //familyMemberParentTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                //treeParentTransform.localScale = new Vector3(0.8f,0.8f,0.8f);                
+                familyMemberParentTransform.localScale = new Vector3(2f, 2f, 2f);
+                treeParentTransform.localScale = new Vector3(0.8f,0.8f,0.8f);                
             }
             else//16:9
             {
                 familyMemberParentTransform.position = new Vector3(-7.6f, 0f, 0);
                 treeParentTransform.position = new Vector3(3f, -0.6f, 0f);
-                //familyMemberParentTransform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
-                //treeParentTransform.localScale = Vector3.one;
+                familyMemberParentTransform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+                treeParentTransform.localScale = Vector3.one;
             }
         }
     }
