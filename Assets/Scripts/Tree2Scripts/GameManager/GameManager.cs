74fecbc085478f3e409add970abe55775c317d5d
Chet2k <chetanr@neelafilm.com>
2025-03-17 14:44:07 +0530

    started positioning of objects

---
+++ Assets/Scripts/Tree2Scripts/GameManager/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMKOC.FamilyTree2
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+        }
+    }
+}
