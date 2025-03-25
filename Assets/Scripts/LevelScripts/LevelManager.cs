84da9e4def4c5f41a146245b453051e8b60f187d
Chet2k <chetanr@neelafilm.com>
2025-03-25 19:13:40 +0530

    marahti remaining

--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -77,7 +77,7 @@ namespace TMKOC.FamilyTree
         }
         private void SetDescriptionLanguageEnum()
         {
-            language = PlayerPrefs.GetString("PlaySchoolLanguage", "English");
+            language = PlayerPrefs.GetString("PlaySchoolLanguage", "Marathi");
             switch (language)
             {
                 case "English":
